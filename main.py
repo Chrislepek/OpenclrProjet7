20fc4f98b079464f094bd4495c7d07c5f2808e6d
Chris Pequignot <chris.lepek@gmail.com>
2025-03-24 09:09:04 +0100

    remonter les fichier à main

---
+++ main.py
@@ -0,0 +1,56 @@
+
+from fastapi import FastAPI, HTTPException
+import pickle
+import numpy as np
+from pydantic import BaseModel
+import pandas as pd
+import threading
+
+# Chemin absolu vers les fichiers
+CSV_PATH = 'C:/Users/Utilisateur/app_train_domain.csv'
+PIPELINE_PATH = 'C:/Users/Utilisateur/pipeline.joblib'
+
+# Charger le fichier CSV
+df = pd.read_csv(CSV_PATH)
+
+# Charger le pipeline
+with open(PIPELINE_PATH, 'rb') as f:
+    pipeline = pickle.load(f)
+
+# FastAPI app
+app = FastAPI()
+
+#Fonction  pour récupérer les données d'un client
+def get_client_data(client_id: int):
+    client_row = df[df['SK_ID_CURR'] == client_id]
+
+    if client_row.empty:
+        return None  # Client ID non trouvé
+
+    # Extraire les features
+    features = client_row.drop(['SK_ID_CURR'], axis=1).values[0]  # - 'client_id'
+    return features
+
+@app.get("/")
+def read_root():
+    return {"message": "API de prédiction de défaut de client"}
+
+@app.get("/predict/{client_id}")
+def predict(client_id: int):
+    # Vérifier si le client existe
+    client_features = get_client_data(client_id)
+
+    if client_features is None:
+        raise HTTPException(status_code=404, detail="Client not found")
+
+    # Préparer les données pour la prédiction
+    data = np.array([client_features])
+
+    # Prédiction du pipeline
+    probabilité = pipeline.predict_proba(data)[:, 1]  # Probabilité de la classe "défaut"
+
+    # Appliquer le seuil optimisé (par exemple, 0.5)
+    seuil = 0.06
+    classe = "Accepté" if probabilité >= seuil else "Refusé"
+
+    return {"client_id": client_id, "probabilité": probabilité[0], "classe": classe}
